3162a001b5e4475c597d81343475d853c036734f
Marianie Vásquez <99088963+Marianie13@users.noreply.github.com>
2025-04-14 16:57:40 -0500

    Division segura

--- Clase.py
+++ Clase.py
@@ -1,4 +1,6 @@
 #App  que permita dividir dos numeros 
+"""
+#Con captura de expeciones
 
 def division_cero(numero1, numero2):
     try:
@@ -8,7 +10,19 @@ def division_cero(numero1, numero2):
         print("La division entre Cero no se puede logar")
         return None
 
-division_cero(2,1)
+division_cero(2,0)
 
-    
+"""
+#Mismo ejercici explicando las execiones multiples
+
+def division_segura():
+    try:
+        numerador = int (input("Ingrese porfavor el numerador de la división: "))
+        denominador = int(input("Ingrese por favor el denominador de la división: "))
+        resultado = numerador / denominador
+        print(f"El resultado de la división de {numerador} entre el {denominador} es igual a: {resultado}")
+    except(ZeroDivisionError, ValueError) as e:
+        print(f"Error {e}")
+        
+division_segura()
     
--- Excepciones.py
+++ Excepciones.py
@@ -8,4 +8,9 @@ finally:
     #Codigo
     
 #Algunas excpeciones comunes:
-
+try:
+        resultado = numero1 / numero2
+        print(f"El resultado es {resultado}")
+    except ZeroDivisionError as e:
+        print("La division entre Cero no se puede logar")
+        return None
