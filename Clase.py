8af6178df49b1ae02caa661106893e0f116107ef
Marianie Vásquez <99088963+Marianie13@users.noreply.github.com>
2025-04-14 20:35:26 -0500

    ejercicos de except

--- Clase.py
+++ Clase.py
@@ -14,6 +14,8 @@ division_cero(2,0)
 
 """
 #Mismo ejercici explicando las execiones multiples
+"""
+
 
 def division_segura():
     try:
@@ -26,3 +28,71 @@ def division_segura():
         
 division_segura()
     
+"""
+#Manejo de expeciones especificas "Exception" no es recomendable siempre por que suele tener errores
+
+"""
+def abrir_archivo():
+    try:
+        with open("daros.txt", "r") as archivo:
+            contenido = archivo.read()
+            numero = int(contenido.strip())
+            print("numero")
+    except Exception as e:
+      print(f"Se produjo un error")
+      
+abrir_archivo()
+
+"""
+#Uso del else y finally
+#Ejercico de division por cero
+"""
+def division_completa():
+    try:
+        numero = int(input("Ingrese un número: "))
+        resultado = 10 / numero 
+        print(f"El resultado de la división es {resultado}")
+    except ValueError as e:
+        print(f"Error: {e}")
+    except ZeroDivisionError as e:
+        print(f"Error {e}")
+    else: 
+        print(f"El resultadode la división es {resultado}")
+    finally:
+        print("La operación finalizo")
+
+division_completa()
+"""
+#App que permite procesar pedidos
+#Validar el codigo del producto sea alfanumerico
+#Validar que la cantidad sea mayor a cero
+
+"""
+def procesar_pedido():
+    try:
+        codigo_producto = input("Ingrese el codigo del producto: ")
+        cantidad = int(input("Ingrese la canridad del producto: "))
+        
+        if not codigo_producto.isalnum():
+             raise ValueError("El codigo del producto debe ser lfanumerico")
+        
+        if not cantidad >= 0:
+            raise ValueError("la cantidad del producto debe ser mayor a cero")
+        
+        precio_unitario = 50
+        total = precio_unitario * cantidad
+    
+    except ValueError as e:
+        print(f"Error al procesar el pedido: {e}")
+    else: 
+        print(f"Total a pagar {total}")
+    finally:
+        print(f"Operacion finalizada")
+    
+procesar_pedido()
+
+"""
+            
+            
+            
+            
